86a75a40191a86f301493eaefef26e177fb630f6
ngodingbentar <dewaqintoro@gmail.com>
2022-09-24 19:39:48 +0700

    added: mapGetters

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,39 +3,43 @@
     <SuspenseComp />
     <VmodelComp />
     <p>likes : {{likes}}</p>
-    <p>fake likes : {{fakeLikes}}</p>
+    <p>fake likes : {{fakeTotalLikes}}</p>
     <p>usernmae : {{username}}</p>
     <pre> user : {{user}}</pre>
   </div>
 </template>
 
 <script>
-import { computed } from 'vue'
-import { useStore } from 'vuex'
+// import { computed } from 'vue'
+import { mapGetters, mapState } from 'vuex'
 import SuspenseComp from '../components/Suspense/index.vue';
 import VmodelComp from '../components/Vmodel/index.vue';
   export default {
     name: 'HomePage',
     components: { SuspenseComp, VmodelComp},
-    setup() {
-      const store = useStore()
-      const likes = computed(() => {
-        return store.state.totalLikes
-      })
-      const fakeLikes = computed(() => {
-        return store.getters.fakeTotalLikes
-      })
-      const username = computed(() => {
-        return store.state.username
-      })
+    // setup() {
+    //   const store = useStore()
+    //   const likes = computed(() => {
+    //     return store.state.totalLikes
+    //   })
+    //   const fakeLikes = computed(() => {
+    //     return store.getters.fakeTotalLikes
+    //   })
+    //   const username = computed(() => {
+    //     return store.state.username
+    //   })
 
-      const user = computed(() => {
-        return store.state.user
-      })
+    //   const user = computed(() => {
+    //     return store.state.user
+    //   })
 
-      return { likes, fakeLikes, username, user}
-      
-    },
+    //   return { likes, fakeLikes, username, user}
+    // },
+    computed: {
+      ...mapState(["username", "user"]),
+      ...mapState({ likes: 'totalLikes' }),
+      ...mapGetters(["fakeTotalLikes"])
+    }
   }
 </script>
   
